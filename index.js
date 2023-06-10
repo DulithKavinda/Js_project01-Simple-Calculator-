b52742f2edd1a1c2eb2561eb9cc5bc4ef6faa1a3
DulithKavinda <121927516+DulithKavinda@users.noreply.github.com>
2023-06-10 05:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function calculate(){
     document.getElementById("result").value=tot;
 };
 
+function calculatesub(){
+    var fnum = document.getElementById("firstNumber").value;
+    var snum = document.getElementById("secondNumber").value;
+    var tot = parseInt(fnum) - parseInt(snum);
+    document.getElementById("result").value=tot;
+};
+
+function calculatemulti(){
+    var fnum = document.getElementById("firstNumber").value;
+    var snum = document.getElementById("secondNumber").value;
+    var tot = parseInt(fnum) * parseInt(snum);
+    document.getElementById("result").value=tot;
+};
+
+function calculatediv(){
+    var fnum = document.getElementById("firstNumber").value;
+    var snum = document.getElementById("secondNumber").value;
+    var tot = parseInt(fnum) / parseInt(snum);
+    document.getElementById("result").value=tot;
+};
